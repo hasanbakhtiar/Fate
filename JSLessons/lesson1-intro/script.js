a80a6c31726bb8f8802fd5113bdfe59df44e131f
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-03-12 19:36:49 +0400

    js  lessons

---
+++ JSLessons/lesson1-intro/script.js
@@ -0,0 +1,16 @@
+//one line comment
+/**
+ * more line comment
+ */
+
+// document.write("Hello2");
+
+// alert("Hello");
+
+// debugger
+// console.log("Hello1");
+// debugger
+// console.log("Hello2");
+// debugger
+// console.log("Hello3");
+// console.log("Hello4");
