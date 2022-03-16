dcd4cb79c54d0a73516d36c1d2528310b736034f
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-03-16 21:08:51 +0400

    operators

---
+++ JSLessons/lesson2-dataType/js/dataType.js
@@ -0,0 +1,67 @@
+
+// 1stGroup Primitive data type - 1ci Qrup Bəsit data tipi
+// string 
+// number
+//boolean
+
+
+//2ndGroup Composite data type - 2ci Qrup birləşik data tipi 
+//array
+//object
+
+//3rdGroup Special data type - 3cuQrup xüsusi data tipi 
+// undefined
+// null
+
+
+// All data type - data tipləri
+
+
+// ==================================================================
+//1stGroup Primitive data type - 1ci Qrup Bəsit data tipi START
+
+// string
+const infoString = 'Hello'; 
+console.log(infoString);
+
+// number 
+const infoNumberTwo = 5;
+const infoNumberOne = 15;
+console.log(infoNumberTwo+infoNumberOne);
+
+// boolean 
+// true | false 
+// console.log(true);
+// console.log(false);
+
+//1stGroup Primitive data type - 1ci Qrup Bəsit data tipi END
+// ==================================================================
+
+// ==================================================================
+//2ndGroup Composite data type - 2ci Qrup birləşik data tipi START
+
+// array
+// const infoArray = new Array();
+const infoArray = [1,"Hello1",true,10,"How a u?"];
+console.log(infoArray);
+
+// object
+// const infoObj = new Object();
+const infoObj = {
+    name: "Kamil",
+    surname:"Kamilov",
+    age: 20
+};
+
+console.log(infoObj.name);
+
+//2ndGroup Composite data type - 2ci Qrup birləşik data tipi END
+// ==================================================================
+
+//3rdGroup Special data type - 3cuQrup xüsusi data tipi START
+// undefined
+const a = undefined;
+console.log(a);
+//null
+console.log(null);
+//3rdGroup Special data type - 3cuQrup xüsusi data tipi END
---
+++ JSLessons/lesson2-dataType/js/operators.js
@@ -0,0 +1,65 @@
+// Opertors
+//1st Arithmetic operator -   Riyazi operatorlar
+
+
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+let infoArithmetic = 15;
+infoArithmetic--;
+console.log(infoArithmetic);
+
+//2nd Assingment opertors - daxil etme(menimsetme) opertorlari
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+let infoAssingment = 15;
+infoAssingment += 20;
+console.log(infoAssingment);
+
+//3rd Logical operators - mentiqi opertorar
+// && - AND (ve)
+// || - OR (ve ya)
+// ! - NOT (deyil)
+
+// if (false || false || true || false) {
+//         console.log("duz");
+// }else{
+//     console.log("sehv");
+// }
+
+
+
+// 4nd Comparison operators - qarsilasdirma operatorlari
+// ==
+// ===
+// <
+// >
+// <=
+// >=
+// !=
+// !==
+
+
+// if ("a" >= "b") {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 5nd string operators - metn opertorlari
+let a  = "JavaScript ";
+ a += "Hello ";
+console.log(a);
