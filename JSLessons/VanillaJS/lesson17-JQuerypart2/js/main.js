d89c95de34673935e7e23db717b3c328dbd2f4b1
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-04-27 17:39:01 +0400

    jquery part2

---
+++ JSLessons/VanillaJS/lesson17-JQuerypart2/js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    $("button").click(function(){
+    //   $('.box').fadeIn("slow");
+    //   $('.box').fadeIn(3000);
+    //   $('.box').fadeIn();
+    //   $('.box').fadeOut(1000);
+    //   $('.box').fadeToggle(1000);
+    //  if (document.querySelector('button').innerHTML == "Jquery test") {
+    //   $('.box').fadeToggle(1000);
+    //   document.querySelector('button').innerHTML = "new";
+         
+    //  }
+    //   $('.box').slideDown(3000);
+    //   $('.box').slideUp("slow");
+    //   $('.box').slideToggle("slow");
+    //   $('.box').animate({left: '250px',width:"toggle"},2000);
+    // let div =$('.box');
+    //   div.animate({height: '300px', opacity: '0.4'}, "slow");
+    //   div.animate({width: '300px', opacity: '0.8'}, "slow");
+    //   div.animate({height: '100px', opacity: '0.4'}, "slow");
+    //   div.animate({width: '100px', opacity: '0.8'}, "slow");
+    // $('.box').css('width',"200px");
+    // $('.box').css({'width':"200px",'height':"200px","transition":"1s"});
+    // document.querySelector('.box').innerHTML = $('p').html()
+    // document.querySelector('.box').innerHTML = $('p').text()
+
+    //   $('.box').text("hello");
+      $('.box').html("<b>hello</b>");
+
+
+    
+    });
+  });
